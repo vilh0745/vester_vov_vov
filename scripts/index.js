a23cb9b2723f1b912435d66633bdbccb835ea250
Kasper Engelbrecht <kasp101y@stud.kea.dk>
2021-05-26 22:01:06 +0200

    Java slides - index

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const slide = document.querySelector("#dias1");
+/*const slide = document.querySelector("#dias1");
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
@@ -10,5 +10,42 @@ function sidenVises() {
 }
 
 function nextSlide() {
-  console.log("virker");
+  console.log("next");
+  
 }
+*/
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+}
+
+var i = 0; // Start Point
+var images = []; // Images Array
+var time = 3000; // Time Between Switch
+
+// Image List
+images[0] = "soundofmetalDias.webp";
+images[1] = "MinariDias.webp";
+images[2] = "MarcoEffekten.webp";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
